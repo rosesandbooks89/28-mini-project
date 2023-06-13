c8feebdbcc919c97c2989c7d1c4b03da15d26ed9
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-06-12 21:30:12 -0500

    update repo with current changes

---
+++ index.js
@@ -0,0 +1,70 @@
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const html = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <div>${name}</div>
+    <div>location</div>
+    <div>bio</div>
+    <div>linkedIn</div>
+    <div>gitHubUrl</div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>`
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Where do you live?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'Tell us about yourself:',
+      name: 'bio',
+    },
+
+    {
+        type: 'input',
+        message: 'Paste your LinkedIn URL:',
+        name: 'linkedIn',
+      },
+      {
+        type: 'input',
+        message: 'Paste your GitHub URL:',
+        name: 'githubUrl',
+      },
+  ])
+    
+
+//   const getUserAnswers = () => {
+//     return inquirer.prompt({"some codey stuff"})
+//   }
+
+//   const otherFunctionThatCompletesThings = () => {
+//     getUserAnswers().then((answers) => do stuff with answers)
+//   }
+
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+    });
+
+   
